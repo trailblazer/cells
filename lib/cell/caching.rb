f5593c59d29724c3048352fcbc7d2ebdf30d2154
Nick Sutterer <apotonick@gmail.com>
2008-10-15 23:09:06 +0200

    caching should basically work.

--- lib/cell/caching.rb
+++ lib/cell/caching.rb
@@ -8,6 +8,9 @@ module Cell::Caching
       extend ClassMethods
       
       alias_method_chain :render_state, :caching
+      
+      cattr_accessor :version_procs
+      base.version_procs= {}  ### DISCUSS: what about per-instance caching definitions?
     end
     
     
@@ -16,18 +19,53 @@ module Cell::Caching
  
  
   module ClassMethods  
-    def cache(state, version_proc=Proc.new{})
+    def cache(state, version_proc = Proc.new{Hash.new})
+      #return unless ActionController::Base.cache_configured?
       
+      version_procs[state] = version_proc
     end
     
+    def cache_store #:nodoc:
+      @cache_store ||= ActionController::Base.cache_store
+    end
     
   end
   
   
   
   def render_state_with_caching(state)
-      render_state_without_caching(state)
-    end
-  
+    key = cache_key(state, self.class.version_procs[state].call)
     
+    # cache hit:
+    return content if content = read_fragment(key)
+    # re-render:
+    return write_fragment(key, render_state_without_caching(state))
+  end
+  
+  
+  def read_fragment(key, cache_options = nil) #:nodoc:
+    returning self.class.cache_store.read(key, cache_options) do |content|
+      @controller.logger.debug "Cell Cache hit: #{key}" unless content.blank?
+    end
+  end
+ 
+  def write_fragment(key, content, cache_options = nil) #:nodoc:
+    @controller.logger.debug "Cell Cache miss: #{key}"
+    self.class.cache_store.write(key, content, cache_options)
+    content
+  end
+  
+  
+  
+  def cache_key(state, args = {}) #:nodoc:
+    key_pieces = [self.class, state]
+    args.sort.each{ |k,v| key_pieces << "#{k}=#{v}" } ### TODO: url_encode, escape, whatever.
+    
+    key = key_pieces.collect { |arg| arg.to_param }.join('/')
+ 
+    ActiveSupport::Cache.expand_cache_key(key, :cells)
+  end
+  
+  
+  
 end
--- test/caching_test.rb
+++ test/caching_test.rb
@@ -8,19 +8,26 @@ require File.dirname(__FILE__) + '/cells/test_cell'
 class CellsCachingTest < Test::Unit::TestCase
   include CellsTestMethods
   
+  def setup
+    super
+    @cc = CachingCell.new(@controller)
+  end
+  
   def self.path_to_test_views
     RAILS_ROOT + "/vendor/plugins/cells/test/views/"
   end
   
   
   def test_caching
-    cell = CachingCell.new(@controller)
-
-    c = cell.render_state(:cached_state)
+    c = @cc.render_state(:cached_state)
     
     assert_equal c, "i should remain the same forever!"
   end
-
+  
+  def test_cache_key
+    assert_equal "cells/CachingCell/some_state", @cc.cache_key(:some_state)
+    assert_equal "cells/CachingCell/some_state/param=9", @cc.cache_key(:some_state, :param => 9)
+  end
 end
 
 class CachingCell < Cell::Base
