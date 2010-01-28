1719ade50154fc193048a7b34593482b278f6e6b
Jonas Grimfelt <grimen@gmail.com>
2010-01-28 05:45:41 +0100

    Added a new generator - cells_install - for generating a default Cells initializer. A good practice.

---
+++ generators/cells_install/templates/initializer.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+if defined?(Cells)
+  # Use this hook to configure Cells.
+  Cells.setup do |config|
+    # Configure view paths that Cells should scan for cell views.
+    # config.view_paths << Rails.root.join('app', 'views')
+  end
+end
