52b3cf5c3cbd43d02c653464d3c71e786abc6987
Nick Sutterer <apotonick@gmail.com>
2010-09-29 13:10:59 +0200

    cleaned up #render and improved (?) docs.

--- lib/cell/rails.rb
+++ lib/cell/rails.rb
@@ -1,5 +1,4 @@
 require 'abstract_controller'
-require 'action_controller'
 
 module Cell
   class Rails < AbstractController::Base
@@ -7,6 +6,7 @@ module Cell
     include AbstractController
     include Rendering, Layouts, Helpers, Callbacks, Translation, Logger
     include ActionController::RequestForgeryProtection
+    
     
     class View < ActionView::Base
       def render(options = {}, locals = {}, &block)
@@ -77,111 +77,97 @@ module Cell
     def self.controller_path
       @controller_path ||= name.sub(/Cell$/, '').underscore unless anonymous?
     end
+
+    # Renders the view for the current state and returns the markup.
+    # Don't forget to return the markup itself from the state method.
+    #
+    # === Options
+    # +:view+::   Specifies the name of the view file to render. Defaults to the current state name.
+    # +:layout+:: Renders the state wrapped in the layout. Layouts reside in <tt>app/cells/layouts</tt>.
+    # +:locals+:: Makes the named parameters available as variables in the view.
+    # +:text+::   Just renders plain text.
+    # +:inline+:: Renders an inline template as state view. See ActionView::Base#render for details.
+    # +:file+::   Specifies the name of the file template to render.
+    # +:nothing+:: Doesn't invoke the rendering process.
+    # +:state+::  Instantly invokes another rendering cycle for the passed state and returns.
+    #
+    # Example:
+    #  class MusicianCell < ::Cell::Base
+    #    def sing
+    #      # ... laalaa
+    #      render
+    #    end
+    #
+    # renders the view <tt>musician/sing.html</tt>.
+    #
+    #    def sing
+    #      # ... laalaa
+    #      render :view => :shout, :layout => 'metal'
+    #    end
+    #
+    # renders <tt>musician/shout.html</tt> and wrap it in <tt>app/cells/layouts/metal.html.erb</tt>.
+    #
+    # === #render is explicit!
+    # You can also alter the markup from #render. Just remember to return it.
+    #
+    #   def sing
+    #     render + render + render
+    #   end
+    #
+    # will render three concated views.
+    #
+    # === Partials?
+    #
+    # In Cells we abandoned the term 'partial' in favor of plain 'views' - we don't need to distinguish
+    # between both terms. A cell view is both, a view and a kind of partial as it represents only a fragment
+    # of the page.
+    #
+    # Just use <tt>:view</tt> and enjoy.
+    def render(opts={})
+      render_view_for(opts, self.action_name)
+    end
+
+    # Climbs up the inheritance chain, looking for a view for the current +state+.
+    def find_family_view_for_state(state)
+      missing_template_exception = nil
+
+      possible_paths_for_state(state).each do |template_path|
+        begin
+          template = find_template(template_path)
+          return template if template
+        rescue ::ActionView::MissingTemplate => missing_template_exception
+        end
+      end
+      
+      raise missing_template_exception
+    end
     
-    # DISCUSS: let @controller point to @parent_controller in views, and @cell is the actual real controller?
-
-      # Renders the view for the current state and returns the markup for the component.
-      # Usually called and returned at the end of a state method.
-      #
-      # ==== Options
-      # * <tt>:view</tt> - Specifies the name of the view file to render. Defaults to the current state name.
-      # * <tt>:template_format</tt> - Allows using a format different to <tt>:html</tt>.
-      # * <tt>:layout</tt> - If set to a valid filename inside your cell's view_paths, the current state view will be rendered inside the layout (as known from controller actions). Layouts should reside in <tt>app/cells/layouts</tt>.
-      # * <tt>:locals</tt> - Makes the named parameters available as variables in the view.
-      # * <tt>:text</tt> - Just renders plain text.
-      # * <tt>:inline</tt> - Renders an inline template as state view. See ActionView::Base#render for details.
-      # * <tt>:file</tt> - Specifies the name of the file template to render.
-      # * <tt>:nothing</tt> - Will make the component kinda invisible and doesn't invoke the rendering cycle.
-      # * <tt>:state</tt> - Instantly invokes another rendering cycle for the passed state and returns.
-      # Example:
-      #  class MyCell < ::Cell::Base
-      #    def my_first_state
-      #      # ... do something
-      #      render
-      #    end
-      #
-      # will just render the view <tt>my_first_state.html</tt>.
-      #
-      #    def my_first_state
-      #      # ... do something
-      #      render :view => :my_first_state, :layout => 'metal'
-      #    end
-      #
-      # will also use the view <tt>my_first_state.html</tt> as template and even put it in the layout
-      # <tt>metal</tt> that's located at <tt>$RAILS_ROOT/app/cells/layouts/metal.html.erb</tt>.
-      #
-      #    def say_your_name
-      #      render :locals => {:name => "Nick"}
-      #    end
-      #
-      # will make the variable +name+ available in the view <tt>say_your_name.html</tt>.
-      #
-      #    def say_your_name
-      #      render :nothing => true
-      #    end
-      #
-      # will render an empty string thus keeping your name a secret.
-      #
-      #
-      # ==== Where have all the partials gone?
-      #
-      # In Cells we abandoned the term 'partial' in favor of plain 'views' - we don't need to distinguish
-      # between both terms. A cell view is both, a view and a kind of partial as it represents only a small
-      # part of the page.
-      # Just use <tt>:view</tt> and enjoy.
-      def render(opts={})
-        render_view_for(opts, self.action_name)
-      end
-
+    # Renders the view belonging to the given state. Will raise ActionView::MissingTemplate
+    # if it can't find a view.
+    def render_view_for(opts, state)
+      return "" if opts[:nothing]
       
-
-      # Climbs up the inheritance hierarchy of the Cell, looking for a view for the current +state+ in each level.
-      def find_family_view_for_state(state)
-        missing_template_exception = nil
-
-        possible_paths_for_state(state).each do |template_path|
-          begin
-            template = find_template(template_path)
-            return template if template
-          rescue ::ActionView::MissingTemplate => missing_template_exception
-          end
-        end
-        
-        raise missing_template_exception
-      end
-      
-      # Renders the view belonging to the given state. Will raise ActionView::MissingTemplate
-      # if it can't find a view.
-      def render_view_for(opts, state)
-        return '' if opts[:nothing]
-
-        ### TODO: dispatch dynamically:
-        if    opts[:text]   ### FIXME: generic option?
-        elsif opts[:inline]
-        elsif opts[:file]
-        elsif opts[:state]  ### FIXME: generic option
-          opts[:text] = render_state(opts[:state])
-        else
-          # handle :layout, :template_format, :view
-          opts = defaultize_render_options_for(opts, state)
-
-          #template    = find_family_view_for_state_with_caching(opts[:view], action_view)
-          template    = find_family_view_for_state(opts[:view])
-          opts[:template] = template
-        end
-
-        opts = sanitize_render_options(opts)
-        
-        render_to_string(opts)
+      rails_options = [:text, :inline, :file]
+      if (opts.keys & rails_options).present?
+      elsif opts[:state]
+        opts[:text] = render_state(opts[:state])
+      else
+        opts            = defaultize_render_options_for(opts, state)
+        template        = find_family_view_for_state(opts[:view])
+        opts[:template] = template
       end
 
-      # Defaultize the passed options from #render.
-      def defaultize_render_options_for(opts, state)
-        opts.reverse_merge!(:view => state)
-      end
-      
-      def sanitize_render_options(opts)
-        opts.except!(:view, :state)
-      end
-		end
+      opts = sanitize_render_options(opts)
+      render_to_string(opts)
+    end
+
+    # Defaultize the passed options from #render.
+    def defaultize_render_options_for(opts, state)
+      opts.reverse_merge!(:view => state)
+    end
+    
+    def sanitize_render_options(opts)
+      opts.except!(:view, :state)
+    end
+  end
 end
