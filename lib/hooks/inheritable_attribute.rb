a349973ca270958b60c3de767f7ac98e03a1f792
Nick Sutterer <apotonick@gmail.com>
2010-10-03 00:33:34 +0200

    added Hooks.inheritable_attr to replace ActiveSupport.

---
+++ lib/hooks/inheritable_attribute.rb
@@ -0,0 +1,32 @@
+module Hooks
+  module InheritableAttribute
+    # Creates an inheritable attribute with accessors in the singleton class. Derived classes inherit the
+    # attributes. This is especially helpful with arrays or hashes that are extended in the inheritance
+    # chain. Note that you have to initialize the inheritable attribute.
+    #
+    # Example:
+    #
+    #   class Cat
+    #     inheritable_attr :drinks
+    #     self.drinks = ["Becks"]
+    #
+    #   class Garfield < Cat
+    #     self.drinks << "Fireman's 4"
+    #
+    # and then, later
+    #
+    #   Garfield.drinks #=> ["Becks, Fireman's 4"]
+    def inheritable_attr(name)
+      instance_eval %Q{
+        def #{name}=(v)
+          @#{name} = v
+        end
+        
+        def #{name}
+          return @#{name} unless superclass.respond_to?(:#{name})
+          @#{name} ||= superclass.#{name}.clone # only do this once.
+        end
+      }
+    end
+  end  
+end
