f5109a4073fb8b70b9a1d94a7f604c0cb3f918a1
Nick Sutterer <apotonick@gmail.com>
2010-10-15 14:22:36 +0200

    cleaned up test/ directory.

--- test/app/helpers/application_helper.rb
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-
-module ApplicationHelper
-  def application_helper_method
-    'global'
-  end
-end
--- test/app/helpers/helper_using_cell_helper.rb
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-
-module HelperUsingCellHelper
-  def an_automatically_included_helper_method
-    'automatic'
-  end
-end
