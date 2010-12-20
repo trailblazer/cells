ba0e81b7d8f34bbcd290538902c025d83d96fb85
Nick Sutterer <apotonick@gmail.com>
2010-12-20 20:09:17 +0100

    added Cell::Base.build to add builders for #render_cell. see the rdocs for more. thanks to Brandon Dimcheff for inspiration.

--- lib/cell.rb
+++ lib/cell.rb
@@ -10,9 +10,51 @@ module Cell
       cell.render_state(state)
     end
     
-    # Creates a cell instance.
+    # Creates a cell instance. Note that this method calls builders which were attached to the
+    # class with Cell::Base.build - this might lead to a different cell being returned.
     def create_cell_for(controller, name, opts={})
-      class_from_cell_name(name).new(controller, opts)
+      build_class_for(controller, class_from_cell_name(name), opts).
+      new(controller, opts)
+    end
+    
+    # Adds a builder to the cell class. Builders are used in #render_cell to find out the concrete
+    # class for rendering. This is helpful if you frequently want to render subclasses according
+    # to different circumstances (e.g. login situations).
+    #
+    # Passes the opts hash from #render_cell into the block. The block is executed in controller context.
+    #
+    # Example:
+    #
+    # Consider two different user box cells in your app.
+    #
+    #   class AuthorizedUserBox < UserInfoBox
+    #   end
+    #
+    #   class AdminUserBox < UserInfoBox
+    #   end
+    #
+    # Now you don't want to have deciders all over your views - use a declarative builder.
+    #
+    #   UserInfoBox.build do |opts|
+    #     AuthorizedUserBox if user_logged_in?
+    #     AdminUserBox if user_is_admin?
+    #   end
+    #
+    # In your view #render_cell will instantiate the right cell for you now.
+    def build(&block)
+      builders << block
+    end
+    
+    def build_class_for(controller, target_class, opts)
+      cell_class = target_class
+      target_class.builders.each { |blk| res = controller.instance_exec(opts, &blk)
+        cell_class = res and break if res
+       }
+      cell_class
+    end
+    
+    def builders
+      @builders ||= []
     end
     
     # Return the default view path for +state+. Override this if you cell has a differing naming style.
--- test/cell_module_test.rb
+++ test/cell_module_test.rb
@@ -1,5 +1,16 @@
 require 'test_helper'
 
+class MusicianCell < Cell::Base
+  
+end
+
+class PianistCell < MusicianCell
+end
+
+class SingerCell < MusicianCell
+end
+
+
 class CellModuleTest < ActiveSupport::TestCase
   include Cell::TestCase::TestMethods
   
@@ -20,9 +31,81 @@ class CellModuleTest < ActiveSupport::TestCase
         assert_equal "Doo", html
         assert flag
       end
+      
+      
     end
     
+    context "create_cell_for" do
+      should "call the cell's builders, eventually returning a different class" do
+        class DrummerCell < BassistCell
+          build do
+            BassistCell
+          end
+        end
+        
+        assert_instance_of BassistCell, Cell::Base.create_cell_for(@controller, "cell_module_test/drummer", :play)
+      end
+    end
     
+    context "calling build" do
+      setup do
+        @controller.class_eval do
+          attr_accessor :bassist
+        end
+        
+        MusicianCell.build do
+          BassistCell if bassist
+        end
+      end
+      
+      teardown do
+        MusicianCell.class_eval do
+          @builders = false
+        end
+      end
+      
+      should "execute the block in controller context" do
+        @controller.bassist = true
+        assert_equal BassistCell,  Cell::Base.build_class_for(@controller, MusicianCell, {})
+      end
+      
+      should "limit the builder to the receiving class" do
+        assert_equal MusicianCell,  Cell::Base.build_class_for(@controller, MusicianCell, {})  # bassist is false.
+        assert_equal PianistCell,   Cell::Base.build_class_for(@controller, PianistCell, {})   # don't inherit anything.
+        @controller.bassist = true
+        assert_equal BassistCell,   Cell::Base.build_class_for(@controller, MusicianCell, {})
+      end
+      
+      should "chain build blocks and execute them in the same order" do
+        MusicianCell.build do
+          PianistCell unless bassist
+        end
+        
+        assert_equal PianistCell, Cell::Base.build_class_for(@controller, MusicianCell, {})  # bassist is false.
+        @controller.bassist = true
+        assert_equal BassistCell, Cell::Base.build_class_for(@controller, MusicianCell, {})
+      end
+      
+      should "stop at the first builder returning a valid cell" do
+        
+      end
+      
+      should "pass options to the block" do
+        BassistCell.build do |opts|
+          SingerCell if opts[:sing_the_song]
+        end
+        assert_equal BassistCell, Cell::Base.build_class_for(@controller, BassistCell, {})
+        assert_equal SingerCell,  Cell::Base.build_class_for(@controller, BassistCell, {:sing_the_song => true})
+      end
+      
+      should "create the original target class if no block matches" do
+        assert_equal PianistCell, Cell::Base.build_class_for(@controller, PianistCell, {})
+      end
+      
+      should "builders should return an empty array per default" do
+        assert_equal [], PianistCell.builders
+      end
+    end
     
     should "provide possible_paths_for_state" do
       assert_equal ["bad_guitarist/play", "bassist/play", "cell/rails/play"], cell(:bad_guitarist).possible_paths_for_state(:play)
