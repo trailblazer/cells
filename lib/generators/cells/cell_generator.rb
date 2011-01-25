da7bdfb6ba7ffecd5f7224f3b15418a5f23716d6
Jorge Calás Lozano <calas@qvitta.net>
2011-01-25 19:03:57 +0100

    Even better, the old generator work but throws message
    * The old generator should be called with the new arguments,
      it's exactly the same as the new one.
    
    * This file should be removed someday I guess.

--- lib/generators/cells/cell_generator.rb
+++ lib/generators/cells/cell_generator.rb
@@ -5,6 +5,13 @@ module Cells
     class CellGenerator < ::Cells::Generators::Base
       source_root File.expand_path('../../templates', __FILE__)
 
+      def create_cell_file
+        template 'cell.rb', File.join('app/cells', class_path, "#{file_name}_cell.rb")
+      end
+
+      hook_for(:template_engine)
+      hook_for(:test_framework)
+
       def say_deprecated
         say "====> This generator is now DEPRECATED. <====", :red
         say "Please use:"
