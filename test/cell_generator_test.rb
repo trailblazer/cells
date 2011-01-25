78e338e25e68f1aa12de0e3d2c2f4457d4fa135f
Jorge Calás Lozano <calas@qvitta.net>
2011-01-25 15:01:51 +0100

    Generator Refactor
    Splits `cells:cell` generator into hookable parts so it can be easy
    to plug new generators (like rspec or slim).
    
    Now it also use and respect rails generators configuration so if you
    have your rails generators tunned for say, use haml instead of erb, the
    cell generator will automatically use the proper generator.
    
    This commit introduces NOT BACKWARD COMPATIBLE modifications:
    
    * The `--haml` option is no longer available.
    * The `-t` option now is compatible with the rest of rails generators,
    	now it is used as alias for `--test-framework`. Use the `-e` option
    	as an alias of `--template-engine`

--- test/cell_generator_test.rb
+++ test/cell_generator_test.rb
@@ -1,18 +1,17 @@
 require 'test_helper'
-
 require 'generators/cells/cell_generator'
 
 class CellGeneratorTest < Rails::Generators::TestCase
   destination File.join(Rails.root, "tmp")
   setup :prepare_destination
   tests ::Cells::Generators::CellGenerator
-   
+
   context "Running script/generate cell" do
     context "Blog post latest" do
+
       should "create the standard assets" do
-        
         run_generator %w(Blog post latest)
-        
+
         assert_file "app/cells/blog_cell.rb", /class BlogCell < Cell::Rails/
         assert_file "app/cells/blog_cell.rb", /def post/
         assert_file "app/cells/blog_cell.rb", /def latest/
@@ -20,12 +19,15 @@ class CellGeneratorTest < Rails::Generators::TestCase
         assert_file "app/cells/blog/post.html.erb", %r(<p>)
         assert_file "app/cells/blog/latest.html.erb", %r(app/cells/blog/latest\.html\.erb)
 
-        assert_file "test/cells/blog_cell_test.rb", %r(class BlogCellTest < Cell::TestCase)
+
+        assert_no_file "app/cells/blog/post.html.haml"
+        assert_no_file "app/cells/blog/post.html.haml"
+        assert_no_file "app/cells/blog/latest.html.haml"
       end
-      
-      should "create haml assets with --haml" do
-        run_generator %w(Blog post latest --haml)
-        
+
+      should "create haml assets with -e haml" do
+        run_generator %w(Blog post latest -e haml)
+
         assert_file "app/cells/blog_cell.rb", /class BlogCell < Cell::Rails/
         assert_file "app/cells/blog_cell.rb", /def post/
         assert_file "app/cells/blog_cell.rb", /def latest/
@@ -33,21 +35,24 @@ class CellGeneratorTest < Rails::Generators::TestCase
         assert_file "app/cells/blog/post.html.haml", %r(%p)
         assert_file "app/cells/blog/latest.html.haml", %r(app/cells/blog/latest\.html\.haml)
 
-        assert_file "test/cells/blog_cell_test.rb"
+
+        assert_no_file "app/cells/blog/post.html.erb"
+        assert_no_file "app/cells/blog/post.html.erb"
+        assert_no_file "app/cells/blog/latest.html.erb"
       end
-      
-      should "create haml assets with -t haml" do
-        run_generator %w(Blog post latest -t haml)
-        
-        assert_file "app/cells/blog_cell.rb", /class BlogCell < Cell::Rails/
-        assert_file "app/cells/blog_cell.rb", /def post/
-        assert_file "app/cells/blog_cell.rb", /def latest/
-        assert_file "app/cells/blog/post.html.haml", %r(app/cells/blog/post\.html\.haml)
-        assert_file "app/cells/blog/post.html.haml", %r(%p)
-        assert_file "app/cells/blog/latest.html.haml", %r(app/cells/blog/latest\.html\.haml)
+
+      should "create test_unit assets with -t test_unit" do
+        run_generator %w(Blog post latest -t test_unit)
 
         assert_file "test/cells/blog_cell_test.rb"
       end
+
+      should "create test_unit assets with -t rspec" do
+        run_generator %w(Blog post latest -t rspec)
+
+        assert_no_file "test/cells/blog_cell_test.rb"
+      end
+
     end
   end
 end
