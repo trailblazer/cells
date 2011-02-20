9bc7bb273bb2439483440272d0dc6a45579592b7
Nick Sutterer <apotonick@gmail.com>
2011-02-20 21:12:07 +0100

    fixed the railtie, sorry.

--- lib/cells/railtie.rb
+++ lib/cells/railtie.rb
@@ -1,17 +1,19 @@
 require "rails/railtie"
 
-class Cells::Railtie < Rails::Railtie
-  initializer "cells.attach_router" do |app|
-    Cell::Rails.class_eval do
-      include app.routes.url_helpers
+module Cells
+  class Railtie < Rails::Railtie
+    initializer "cells.attach_router" do |app|
+      Cell::Rails.class_eval do
+        include app.routes.url_helpers
+      end
     end
-  end
-  
-  initializer "cells.setup_view_paths" do |app|
-    Cell::Base.setup_view_paths!
-  end
-  
-  rake_tasks do
-    load "cells/cells.rake"
+    
+    initializer "cells.setup_view_paths" do |app|
+      Cell::Base.setup_view_paths!
+    end
+    
+    rake_tasks do
+      load "cells/cells.rake"
+    end
   end
 end
