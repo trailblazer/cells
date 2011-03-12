3afac86f301fac2fd0bf1669e871eff4506caa3f
Stafford Brunk <wingrunr21@gmail.com>
2011-03-12 12:55:12 -0500

    Add a root_url route to test rails app and test that cells can now use *_url helpers

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,4 @@
 Dummy::Application.routes.draw do |map|
   match ':controller(/:action(/:id(.:format)))'
+  root :to => 'musician#index'
 end
--- test/rails/router_test.rb
+++ test/rails/router_test.rb
@@ -7,25 +7,38 @@ module ApplicationTests
         BassistCell.class_eval do
           def promote; render; end
         end
-      
+
         #assert ::Cell::Rails.view_context_class._routes, "Cells::Railtie initializer wasn't invoked."
         #assert ! ::OmgController.new.respond_to?( :render_cell)
-        
+
         get "index"
         assert_response :success
         assert_equal "Find me at <a href=\"/musician\">vd.com</a>", @response.body
       end
-      
+
+      should "allow cells to use *_url helpers" do
+        BassistCell.class_eval do
+          def promote_again; render; end
+        end
+
+        #assert ::Cell::Rails.view_context_class._routes, "Cells::Railtie initializer wasn't invoked."
+        #assert ! ::OmgController.new.respond_to?( :render_cell)
+
+        get "promote"
+        assert_response :success
+        assert_equal "Find me at <a href=\"http://test.host/\">vd.com</a>", @response.body
+      end
+
       should "allow cells to use #config" do
-        
+
         BassistCell.class_eval do
           def provoke; render; end
         end
-        
+
         get "promotion"
         assert_response :success
         assert_equal "That's me, naked <img alt=\"Me\" src=\"/images/me.png\" />", @response.body
-      end 
+      end
     end
   end
 end
