62c66d553657dbc646b17c6d4a7c6d77dafdb99a
Stafford Brunk <wingrunr21@gmail.com>
2011-03-12 12:55:12 -0500

    Add a root_url route to test rails app and test that cells can now use *_url helpers

--- test/dummy/app/controllers/musician_controller.rb
+++ test/dummy/app/controllers/musician_controller.rb
@@ -2,31 +2,35 @@ class MusicianController < ActionController::Base
   def index
     render :text => render_cell(:bassist, :promote)
   end
-            
+
+  def promote
+    render :text => render_cell(:bassist, :promote_again)
+  end
+
   def promotion
     render :text => render_cell(:bassist, :provoke)
   end
-  
+
   def featured
   end
-  
+
   def featured_with_block
   end
-  
+
   def skills
     render :text => render_cell(:bassist, :listen)
   end
-  
+
   def hamlet
   end
-  
+
   attr_reader :flag
   def promotion_with_block
     html = render_cell(:bassist, :play) do |cell|
       @flag = cell.class
     end
-    
-    render :text => html 
+
+    render :text => html
   end
-  
+
 end
