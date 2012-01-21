5bc90f2d4cfc42e6506970a43a213ba620f5b422
Viacheslav Molokov <viacheslav.molokov@gmail.com>
2012-01-21 17:09:02 +0400

    In Rails 3.2 view_paths is PathSet. paths are in view_paths.paths

--- test/cells_module_test.rb
+++ test/cells_module_test.rb
@@ -6,20 +6,20 @@ class CellsModuleTest < ActiveSupport::TestCase
       setup do
         @old_view_paths = Cell::Rails.view_paths.clone
       end
-      
+
       teardown do
         Cell::Rails.view_paths = @old_view_paths
       end
-      
+
       should "provide .setup" do
         Cells.setup do |c|
           c.append_view_path "/road/to/nowhere"
         end
-        
-        assert_equal "/road/to/nowhere", Cell::Rails.view_paths.last.to_s
+
+        assert_equal "/road/to/nowhere", Cell::Rails.view_paths.paths.last.to_s
       end
     end
-    
+
     should "respond to #rails3_1_or_more?" do
       if Rails::VERSION::MINOR == 0
         assert ! Cells.rails3_1_or_more?
