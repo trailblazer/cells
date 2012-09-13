cedeb6d5e013cc93c52a77e0aa7645daf8273237
Nick Sutterer <apotonick@gmail.com>
2012-09-13 16:18:00 +0200

    adding a test to render a global app partial from a cell.

--- test/rails/view_test.rb
+++ test/rails/view_test.rb
@@ -4,6 +4,7 @@ class RailsViewTest < ActiveSupport::TestCase
   include Cell::TestCase::TestMethods
   
   context "A cell view" do
+    # DISCUSS: should we allow :partial from a state, too?
     context "calling render :partial" do
       should "render the local cell partial in bassist/dii" do
         assert_equal("Dii", in_view(:bassist) do
@@ -14,6 +15,16 @@ class RailsViewTest < ActiveSupport::TestCase
       should "render the foreign cell partial in bad_guitarist/dii" do
         assert_equal("Dooom", in_view(:bassist) do
           render :partial => "bad_guitarist/dii"
+        end)
+      end
+
+
+      should "render the global partial app/views/shared/dong" do
+        class PercussionistCell < BassistCell
+          append_view_path("test/app/views")
+        end
+        assert_equal("Dong!", in_view("rails_view_test/percussionist") do
+          render :partial => "shared/dong"
         end)
       end
     end
