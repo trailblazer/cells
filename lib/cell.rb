cfe1d61208b14346c88966a63bd13305a99bc3b6
Nick Sutterer <apotonick@gmail.com>
2013-10-16 11:55:51 +1100

    handle both model and options constructor in #process_args for now.

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,12 +1,27 @@
 module Cell
   module OptionsConstructor
+  private
     def process_args(options={})
+      if options.is_a?(Hash) # TODO: i don't like this too much.
+        process_options(options)
+      else
+        process_model(options)
+      end
+
+      super # Base.
+    end
+
+    # DISCUSS: have 2 classes for that?
+
+    def process_options(options)
       options.each do |k, v|
         instance_variable_set("@#{k}", v)
         singleton_class.class_eval { attr_reader k }
       end
+    end
 
-      super # Base.
+    def process_model(model)
+      @model = model
     end
   end
 end
