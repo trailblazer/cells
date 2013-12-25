20a664eac48908e238c3e04251c167b613fb92fb
Nick Sutterer <apotonick@gmail.com>
2013-12-25 12:37:30 +1100

    set the Layouts constant for all rails 3.0-4.0.

--- lib/cell/rails3_0_strategy.rb
+++ lib/cell/rails3_0_strategy.rb
@@ -1,21 +1,23 @@
 # This file contains VersionStrategies for the Cell and Cells module for Rails 3.0.
 module Cell
+  Layouts = AbstractController::Layouts
+
   # Methods to be included in Cell::Rails in 3.0 context, where there's no view inheritance.
   module VersionStrategy
     extend ActiveSupport::Concern
-    
+
     class MissingTemplate < ActionView::ActionViewError
       def initialize(message, possible_paths)
         super(message + " and possible paths #{possible_paths}")
       end
     end
-    
-    
+
+
     module ClassMethods
       def helper_modules
         [_helpers, _routes.url_helpers]
       end
-      
+
       # Return the default view path for +state+. Override this if you cell has a differing naming style.
       def view_for_state(state)
         "#{cell_name}/#{state}"
@@ -33,13 +35,13 @@ module Cell
         controller_path
       end
     end
-    
+
   private
     # Computes all possible paths for +state+ by traversing up the inheritance chain.
     def possible_paths_for_state(state)
       self.class.find_class_view_for_state(state).reverse!
     end
-    
+
     # Climbs up the inheritance chain, looking for a view for the current +state+.
     def find_family_view_for_state(state)
       exception       = nil
@@ -55,10 +57,10 @@ module Cell
 
       raise MissingTemplate.new(exception.message, possible_paths)
     end
-    
+
     def process_opts_for(opts, state)
       lookup_context.formats = opts.delete(:format) if opts[:format]
-      
+
       opts[:template] = find_family_view_for_state(opts.delete(:view) || state)
     end
   end
@@ -71,7 +73,7 @@ module Cells
       def registered_engines
         ::Rails.application.railties.engines
       end
-      
+
       def existent_directories_for(path)
         path.to_a.select { |d| File.directory?(d) }
       end
