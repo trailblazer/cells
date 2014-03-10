2fb531e0f0bccfe2d1b0cc02bab594e1a6cf2645
Nick Sutterer <apotonick@gmail.com>
2014-03-10 20:41:07 +1100

    finalize completion of caching section in README and remove it from the actual class.

--- lib/cell/caching.rb
+++ lib/cell/caching.rb
@@ -19,54 +19,6 @@ module Cell
     end
 
     module ClassMethods
-      # Caches the rendered view of +state+.
-      #
-      # Examples:
-      #
-      # This will cache forever.
-      #
-      #   class CartCell < Cell::Base
-      #     cache :show
-      #
-      # You can also pass options to the caching engine as known from Rails caching.
-      #
-      #   cache :show, :expires_in => 10.minutes
-      #
-      # Options are dynamically evaluated at render-time when you pass in a lambda.
-      #
-      #   cache :show, tags: lambda { |*args| cell.tags }
-      #
-      # As the options are passed directly into the cache store, this is useful when using rails-cache-tags.
-      #
-      # The +:if+ option lets you define a conditional proc or instance method. If it doesn't
-      # return a true value, caching for that state is skipped.
-      #
-      #   cache :show, :if => lambda { |options| options[:enable_cache] }
-      #
-      # If you need your own granular cache keys, pass a versioner block.
-      #
-      #   cache :show do |options|
-      #     "user/#{options[:id]}"
-      #   end
-      #
-      # All blocks are executed in the cell instance context, allowing you to call methods or access instance
-      # variables.
-      #
-      # This will result in a cache key like <tt>cells/cart/show/user/1</tt>.
-      #
-      # Alternatively, use an instance method.
-      #
-      #   cache :show, :versioner
-      #   def versioner(options)
-      #     "user/#{options[:id]}"
-      #   end
-      #
-      # Two things to mention here.
-      # * The return value of the method/block is <em>appended</em> to the state cache key.
-      # * You may return a string, a hash, an array, ActiveSupport::Caching will compile it.
-      #
-      # == Inheritance
-      # Please note that cache configuration is inherited to derived cells.
       def cache(state, *args, &block)
         options = args.extract_options!
 
