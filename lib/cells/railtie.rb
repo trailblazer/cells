28bce927eba696b1e5467dbc1374f8001bc69326
Nick Sutterer <apotonick@gmail.com>
2014-04-15 15:59:56 +1000

    allow packaging assets (javascript, css, images, ...) into the cells' assets/ directory, which is fucking awesome!

--- lib/cells/railtie.rb
+++ lib/cells/railtie.rb
@@ -2,20 +2,37 @@ require "rails/railtie"
 
 module Cells
   class Railtie < ::Rails::Railtie
+    config.cells = ActiveSupport::OrderedOptions.new
+
+
     initializer "cells.attach_router" do |app|
       Cell::Base.class_eval do
         include app.routes.url_helpers
       end
     end
-    
+
     initializer "cells.setup_view_paths" do |app|
       Cell::Base.setup_view_paths!
     end
-    
+
     initializer "cells.setup_engines_view_paths" do |app|
       Cells::Engines.append_engines_view_paths_for(app.config.action_controller)
     end
-    
+
+    add_paths_block = lambda do |app|
+      (app.config.cells.with_assets or []).each do |name|
+        # FIXME: this doesn't take engine cells into account.
+        app.config.assets.paths << "#{app.root}/app/cells/#{name}/assets"
+      end
+    end
+
+    # Standard initializer
+    initializer 'cells.update_asset_paths', &add_paths_block
+
+    # run at assets:precompile even when `config.assets.initialize_on_precompile = false`
+    initializer 'cells.update_asset_paths', :group => :assets, &add_paths_block
+
+
     rake_tasks do
       load "cells/cells.rake"
     end
