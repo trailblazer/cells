7f3e3e662d34ecf804d442df856652d31714d747
Nick Sutterer <apotonick@gmail.com>
2014-05-06 10:30:31 +1000

    fix prefixes_test.

--- test/prefixes_test.rb
+++ test/prefixes_test.rb
@@ -28,18 +28,18 @@ class PrefixesTest < MiniTest::Spec
 
   describe "::controller_path" do
     it { ::BassistCell.new(@controller).controller_path.must_equal "bassist" }
-    it { SingerCell.new(@controller).controller_path.must_equal "base_test/singer" }
+    it { SingerCell.new(@controller).controller_path.must_equal "prefixes_test/singer" }
   end
 
   describe "#_prefixes" do
     it { ::BassistCell.new(@controller)._prefixes.must_equal  ["bassist"] }
-    it { SingerCell.new(@controller)._prefixes.must_equal   ["base_test/singer"] }
-    it { BackgroundVocalsCell.new(@controller)._prefixes.must_equal ["base_test/background_vocals", "base_test/singer"] }
-    it { ChorusCell.new(@controller)._prefixes.must_equal   ["base_test/chorus", "base_test/background_vocals", "base_test/singer"] }
+    it { SingerCell.new(@controller)._prefixes.must_equal   ["prefixes_test/singer"] }
+    it { BackgroundVocalsCell.new(@controller)._prefixes.must_equal ["prefixes_test/background_vocals", "prefixes_test/singer"] }
+    it { ChorusCell.new(@controller)._prefixes.must_equal   ["prefixes_test/chorus", "prefixes_test/background_vocals", "prefixes_test/singer"] }
 
-    it { GuitaristCell.new(@controller)._prefixes.must_equal ["stringer", "base_test/singer"] }
-    it { BassistCell.new(@controller)._prefixes.must_equal ["base_test/bassist", "basser", "base_test/singer"] }
-    # it { DrummerCell.new(@controller)._prefixes.must_equal ["drummer", "stringer", "base_test/singer"] }
+    it { GuitaristCell.new(@controller)._prefixes.must_equal ["stringer", "prefixes_test/singer"] }
+    it { BassistCell.new(@controller)._prefixes.must_equal ["prefixes_test/bassist", "basser", "prefixes_test/singer"] }
+    # it { DrummerCell.new(@controller)._prefixes.must_equal ["drummer", "stringer", "prefixes_test/singer"] }
   end
 
   # it { Record::Cell.new(@controller).render_state(:show).must_equal "Rock on!" }
