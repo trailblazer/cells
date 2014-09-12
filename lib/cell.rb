15753c45ce20ee0d78e4fc569cba02533510f241
Nick Sutterer <apotonick@gmail.com>
2014-09-12 10:21:10 +1000

    move ::rails_version into Cell and remove OptionsConstructor.

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,31 +1,6 @@
 module Cell
-  module OptionsConstructor
-    def self.included(base)
-      warn "[Cells] Cell::OptionsConstructor is deprecated. Please do not use it anymore and switch to ViewModel."
-    end
-
-  private
-    def process_args(options={})
-      if options.is_a?(Hash) # TODO: i don't like this too much.
-        process_options(options)
-      else
-        process_model(options)
-      end
-
-      super # Base.
-    end
-
-    # DISCUSS: have 2 classes for that?
-
-    def process_options(options)
-      options.each do |k, v|
-        instance_variable_set("@#{k}", v)
-        singleton_class.class_eval { attr_reader k }
-      end
-    end
-
-    def process_model(model)
-      @model = model
-    end
+  require 'uber/version'
+  def self.rails_version
+    Uber::Version.new(::ActionPack::VERSION::STRING)
   end
 end
