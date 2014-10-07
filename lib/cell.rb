1d7a33e2a804ce70299a8609e682af0a250a2ae4
Nick Sutterer <apotonick@gmail.com>
2014-10-08 09:24:47 +1100

    whoops.

--- lib/cell.rb
+++ lib/cell.rb
@@ -28,8 +28,5 @@ require 'cell/view_model'
 require 'cell/concept'
 # require 'cells/engines'
 
-<<<<<<< HEAD
 require 'cell/railtie'
-=======
 require 'cell/railtie'
->>>>>>> 6618c7f04d99e51f2b383b99384d251f90c021cd
