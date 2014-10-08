c564c5d825b2183ae8199dd2d726ec73a0f29459
Ryan Selk <ryanselk@gmail.com>
2014-10-08 16:35:48 -0600

    remove ActiveSupport::TestCase.class_eval do

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,20 +19,6 @@ end
 class BassistCell < Cell::ViewModel
 end
 
-
-ActiveSupport::TestCase.class_eval do # this is only needed in integration tests (AC::TestCase).
-  def fix_relative_url_root
-    return unless Cell.rails_version.~("3.0")
-
-    @controller.config.instance_eval do
-      def relative_url_root
-        ""
-      end
-    end
-end
-end
-
-
 class MusicianController < ActionController::Base
   def view_with_concept_with_show
     render :inline => %{<%= concept("view_extensions_test/cell", "Up For Breakfast", volume: 1).show %>} # TODO: concept doesn't need .call
