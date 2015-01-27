7d8a3945767a809ba503e7d7a3d4d4979245bd9f
Nick Sutterer <apotonick@gmail.com>
2015-01-27 13:20:25 +1100

    allow passing render :partial and :format.

--- lib/cell/partial.rb
+++ lib/cell/partial.rb
@@ -1,3 +1,6 @@
+# Allows to render global partials, for example.
+#
+#   render partial: "../views/shared/container"
 module Cell::ViewModel::Partial
   def template_for(view, engine)
     base      = self.class.view_paths
@@ -7,11 +10,12 @@ module Cell::ViewModel::Partial
     prefixes  = [parts.join("/")]
 
     # self.class.templates[base, [parts.join("/")], "_#{view}.html", engine] or raise Cell::ViewModel::TemplateMissingError.new(base, _prefixes, view, engine, nil)
-    self.class.templates[base, prefixes, view, engine] or raise Cell::ViewModel::TemplateMissingError.new(base, prefixes, view, engine, nil)
+    self.class.templates[base, prefixes, view, engine] or raise Cell::TemplateMissingError.new(base, prefixes, view, engine, nil)
   end
 
   def process_options!(options)
     super
-    options.merge!( {:view => options[:partial]}) if options[:partial]
+    options.merge!(:view => options[:partial]) if options[:partial]
+    options[:view] += ".#{options[:format]}" if options[:format]
   end
 end
--- test/partial_test.rb
+++ test/partial_test.rb
@@ -11,7 +11,12 @@ class PartialTest < MiniTest::Spec
     def show
       render partial: "../fixtures/partials/show.html"
     end
+
+    def show_with_format
+      render partial: "../fixtures/partials/show", format: :xml
+    end
   end
 
   it { WithPartial.new(nil).show.must_equal "I Am Wrong And I Am Right" }
+  it { WithPartial.new(nil).show_with_format.must_equal "<xml>I Am Wrong And I Am Right</xml>" }
 end
