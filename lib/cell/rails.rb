a428dc4b91b022f8e9d9d831b63e734729b896e7
Nick Sutterer <apotonick@gmail.com>
2015-05-25 19:01:00 +1000

    remove old comments.

--- lib/cell/rails.rb
+++ lib/cell/rails.rb
@@ -1,5 +1,4 @@
-# These Methods are automatically added to all Controllers and Views when
-# the cells plugin is loaded.
+# These methods are automatically added to all controllers and views.
 module Cell
   module RailsExtensions
     module ActionController
@@ -10,44 +9,6 @@ module Cell
       def concept(name, *args, &block)
         Concept.cell(name, self, *args, &block)
       end
-
-      # # Renders the cell state and returns the content. You may pass options here, too. They will be
-      # # around in @opts.
-      # #
-      # # Example:
-      # #
-      # #   @box = render_cell(:posts, :latest, :user => current_user)
-      # #
-      # # If you need the cell instance before it renders, you can pass a block receiving the cell.
-      # #
-      # # Example:
-      # #
-      # #   @box = render_cell(:comments, :top5) do |cell|
-      # #     cell.markdown! if config.parse_comments?
-      # #   end
-      # def render_cell(name, state, *args, &block)
-      #   ::Cell::Rails.render_cell(name, state, self, *args, &block)
-      # end
-
-      # # Expires the cached cell state view, similar to ActionController::expire_fragment.
-      # # Usually, this method is used in Sweepers.
-      # # Beside the obvious first two args <tt>cell_name</tt> and <tt>state</tt> you can pass
-      # # in additional cache key <tt>args</tt> and cache store specific <tt>opts</tt>.
-      # #
-      # # Example:
-      # #
-      # #  class ListSweeper < ActionController::Caching::Sweeper
-      # #   observe List, Item
-      # #
-      # #   def after_save(record)
-      # #     expire_cell_state :my_listing, :display_list
-      # #   end
-      # #
-      # # will expire the view for state <tt>:display_list</tt> in the cell <tt>MyListingCell</tt>.
-      # def expire_cell_state(cell_class, state, args={}, opts=nil)
-      #   key = cell_class.state_cache_key(state, args)
-      #   cell_class.expire_cache_key(key, opts)
-      # end
     end
 
     module ActionView
