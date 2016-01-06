6aa3b6651e28c1e81c2495dca60d7d8303be1248
John Doe <author@example.com>
2016-01-06 11:49:55 +0300

    Fix: Escaped.property does not escape multiple properties

--- lib/cell/escaped.rb
+++ lib/cell/escaped.rb
@@ -4,20 +4,23 @@ module Cell::ViewModel::Escaped
   end
 
   module Property
-    def property(name, *args)
-      super.tap do # super defines #title
-        mod = Module.new do
-          define_method(name) do |options={}|
-            value = super() # call the original #title.
-            return value unless value.is_a?(String)
-            return value if options[:escape] == false
-            escape!(value)
+    def property *names
+      super.tap do
+        prepend Module.new {
+          names.flatten!
+          names.each do |name|
+            module_eval <<-RUBY, __FILE__, __LINE__ + 1
+              def #{name}(escape: true)
+                value = super()
+                return value unless value.is_a?(String) || escape
+                escape! value
+              end
+            RUBY
           end
-        end
-        include mod
+        }
       end
     end
-  end # Property
+  end
 
   # Can be used as a helper in the cell, too.
   # Feel free to override and use a different escaping implementation.
