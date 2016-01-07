8c81ad8e7e70a55df83100f3365d11c9269761d7
James Logsdon <dwarf@girsbrain.org>
2016-01-07 01:40:54 -0500

    Escaped.property now accepts multiple property names to remain consistent with the default property method. Fixes #359.

--- test/property_test.rb
+++ test/property_test.rb
@@ -20,6 +20,7 @@ class EscapedPropertyTest < MiniTest::Spec
     include Escaped
     property :title
     property :artist
+    property :copyright, :lyrics
 
     def title(*)
       "#{super}</b>" # super + "</b>" still escapes, but this is Rails.
@@ -30,10 +31,16 @@ class EscapedPropertyTest < MiniTest::Spec
     end
   end
 
-  let (:song) { Struct.new(:title, :artist).new("<b>She Sells And Sand Sandwiches", Object) }
+  let (:song) do
+    Struct
+      .new(:title, :artist, :copyright, :lyrics)
+      .new("<b>She Sells And Sand Sandwiches", Object, "<a>Copy</a>", "<i>Words</i>")
+  end
 
   # ::property escapes, everywhere.
   it { SongCell.(song).title.must_equal "&lt;b&gt;She Sells And Sand Sandwiches</b>" }
+  it { SongCell.(song).copyright.must_equal "&lt;a&gt;Copy&lt;/a&gt;" }
+  it { SongCell.(song).lyrics.must_equal "&lt;i&gt;Words&lt;/i&gt;" }
   # no escaping for non-strings.
   it { SongCell.(song).artist.must_equal Object }
   # no escaping when escape: false
