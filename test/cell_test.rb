12f366edb47db5b23c2afc469e68c0d88f60ad18
Celso Fernandes <fernandes@zertico.com>
2016-04-28 13:46:49 -0300

    Use render with a block

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -2,7 +2,15 @@ require 'test_helper'
 
 class CellTest < MiniTest::Spec
   class SongCell < Cell::ViewModel
+    self.view_paths = ['test/fixtures']
+
     def show
+    end
+
+    def show_with_block(&block)
+      render do
+        block
+      end
     end
   end
   # ::rails_version
@@ -10,4 +18,7 @@ class CellTest < MiniTest::Spec
 
   # #options
   it { SongCell.new(nil, genre: "Punkrock").send(:options)[:genre].must_equal "Punkrock" }
+
+  # #block
+  it { SongCell.new(nil, genre: "Punkrock").(:show_with_block) { "hello" }.must_equal "<b>hello</b>" }
 end
