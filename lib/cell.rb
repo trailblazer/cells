2a295b05ce1d8f6151de5eaefea105017a297ffe
Nick Sutterer <apotonick@gmail.com>
2023-08-22 07:26:10 +0200

    allow yield. why is this called `yield` here, anyway? isn't this just another variable being passed into the template rendering?

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,12 +1,12 @@
 require "tilt/template"
 
 class Cell
-  def self.call(private_options={}, **options)
+  def self.call(private_options={}, **options, &block)
     # raise private_options.inspect
-    render(**private_options)
+    render(**private_options, &block)
   end
 
-  def self.render(template:, exec_context: nil)
-    template.render(exec_context)
+  def self.render(template:, exec_context: nil, &block)
+    template.render(exec_context, &block)
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -16,5 +16,9 @@ class CellTest < Minitest::Spec
     template = Cell::Erb::Template.new("test/show_title.erb")
     result = Cell.({template: template, exec_context: Struct.new(:title).new("Epic!")})
     assert_equal result, %(title: Epic!\n)
+
+    template = Cell::Erb::Template.new("test/show_yield.erb")
+    result = Cell.({template: template, exec_context: nil}) { "insert me!" }
+    assert_equal result, %(yield: insert me!\n)
   end
 end
