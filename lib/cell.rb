3e87c147b0595555ef4b1e929da8d18f896bece3
Nick Sutterer <apotonick@gmail.com>
2023-08-25 17:50:44 +0200

    make `Cell.call` always return a `Result`. this implies that exec_context needs to implement `#to_h`.

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,12 +1,56 @@
 require "tilt/template"
 
 class Cell
+
   def self.call(private_options={}, **options, &block)
-    # raise private_options.inspect
-    render(**private_options, &block)
+    html = render_template(**private_options, &block)
+
+    exec_context = private_options[:exec_context] # DISCUSS: swap positional and kw?
+
+    # DISCUSS: make this optional?
+    Result.new(
+      content: html,
+      **exec_context.to_h,
+    ).freeze
   end
 
-  def self.render(template:, exec_context: nil, &block)
+  class Result
+    def initialize(content:, **options)
+      @content = content
+      @options = options
+    end
+
+    def to_s
+      @content
+    end
+
+    def to_h
+      @options
+    end
+  end
+
+  # Tilt-specific
+  def self.render_template(template:, exec_context: nil, &block)
     template.render(exec_context, &block)
   end
 end
+
+
+
+
+
+# def self.call(template:, exec_context:)
+#   # html = super
+#   # DISCUSS: do we need exec_context.() ? yes, to dispatch to different views, maybe?
+#   _result = exec_context.(template: template) # returns {Result}.
+# end
+
+# def call(template:, &block)
+#   html = ::Cell.render(template: template, exec_context: self, &block)
+
+#   # DISCUSS: make this optional?
+#   ::Cell::Result.new(
+#     content: html,
+#     **to_h,
+#   ).freeze
+# end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -8,17 +8,27 @@ require "cells/__erb__"
 
 
 class CellTest < Minitest::Spec
-  it "what" do
+  it "low-level API" do
     template = Cell::Erb::Template.new("test/show.erb")
     result = Cell.({template: template})
-    assert_equal result, %(<div>show</div>\n)
+    assert_equal result.to_s, %(<div>show</div>\n)
 
     template = Cell::Erb::Template.new("test/show_title.erb")
     result = Cell.({template: template, exec_context: Struct.new(:title).new("Epic!")})
-    assert_equal result, %(title: Epic!\n)
+    assert_equal result.to_s, %(title: Epic!\n)
 
     template = Cell::Erb::Template.new("test/show_yield.erb")
     result = Cell.({template: template, exec_context: nil}) { "insert me!" }
-    assert_equal result, %(yield: insert me!\n)
+    assert_equal result.to_s, %(yield: insert me!\n)
+  end
+
+  it "can return variables in Result" do
+    exec_context = {headers: []} # {exec_context} needs to expose {#to_h} (which is implemented in almost any object, anyway)
+
+    template = Cell::Erb::Template.new("test/show.erb")
+    result = Cell.({template: template, exec_context: exec_context})
+
+    assert_equal result.to_s, %(<div>show</div>\n)
+    assert_equal result.to_h.inspect, %({:headers=>[]})
   end
 end
